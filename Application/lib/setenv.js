9e63f8cc25a749cb41e84185cca6e9d9551a3495
Hiroki Watanabe <hirokit.watanabe@ntt.com>
2021-01-15 12:51:50 +0900

    change double quote to single quote in environment variable

--- Application/lib/setenv.js
+++ Application/lib/setenv.js
@@ -1,5 +1,5 @@
 exports.config = (env) => {
-  const { TS_HOST, TS_API_HOST, TS_CLIENT_ID, TS_CLIENT_SECRET } = JSON.parse(env.TS_CONNECTION)
+  const { TS_HOST, TS_API_HOST, TS_CLIENT_ID, TS_CLIENT_SECRET } = JSON.parse(env.TS_CONNECTION.replace(/'/g, '"'))
   process.env.TS_HOST = TS_HOST
   process.env.TS_API_HOST = TS_API_HOST
   process.env.TS_CLIENT_ID = TS_CLIENT_ID
@@ -7,20 +7,20 @@ exports.config = (env) => {
   // process.env.TS_APP_VERSION
   // TS_APP_VERSIONはアプリのバージョンアップに従い変更頻度が高いためKey Vault管理しない
 
-  const { DB_HOST, DB_NAME, DB_USER, DB_PASS } = JSON.parse(env.DB_CONNECTION)
+  const { DB_HOST, DB_NAME, DB_USER, DB_PASS } = JSON.parse(env.DB_CONNECTION.replace(/'/g, '"'))
   process.env.DB_HOST = DB_HOST
   process.env.DB_NAME = DB_NAME
   process.env.DB_USER = DB_USER
   process.env.DB_PASS = DB_PASS
 
-  const { REDIS_HOST, REDIS_PORT, REDIS_PASS } = JSON.parse(env.REDIS_CONNECTION)
+  const { REDIS_HOST, REDIS_PORT, REDIS_PASS } = JSON.parse(env.REDIS_CONNECTION.replace(/'/g, '"'))
   process.env.REDIS_HOST = REDIS_HOST
   process.env.REDIS_PORT = REDIS_PORT
   process.env.REDIS_PASS = REDIS_PASS
   // process.env.REDIS_TTL
   // REDIS_TTLは変更の可能性があるためKey Vault管理しない
 
-  const { TOKEN_ENC_PASS, TOKEN_ENC_SALT } = JSON.parse(env.TOKEN_ENC)
+  const { TOKEN_ENC_PASS, TOKEN_ENC_SALT } = JSON.parse(env.TOKEN_ENC.replace(/'/g, '"'))
   process.env.TOKEN_ENC_PASS = TOKEN_ENC_PASS
   process.env.TOKEN_ENC_SALT = TOKEN_ENC_SALT
 }
