9adf99df6d4e47ae5436f7aae38890297d5b89a9
jinho.kim <jinho.kim@eclnttcom000670.onmicrosoft.com>
2021-07-09 14:24:08 +0900

    ST#846 サポート画面修正（金・姜）

---
+++ Application/controllers/contractController.js
@@ -0,0 +1,20 @@
+const Contract = require('../models').Contract
+const logger = require('../lib/logger')
+
+module.exports = {
+  findOne: async (tenantId) => {
+    try {
+      const contract = await Contract.findOne({
+        where: {
+          tenantId: tenantId
+        }
+      })
+
+      return contract
+    } catch (error) {
+      // status 0はDBエラー
+      logger.error({ user: tenantId, stack: error.stack, status: 0 }, error.name)
+      return error
+    }
+  }
+}
