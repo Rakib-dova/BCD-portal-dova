ba85bc967d696316f24b6c4faa18e85850515abc
hirohashi <m.hirohashi@ntt.com>
2021-07-29 16:17:46 +0900

    PB#484_ST#1094対応

--- Application/routes/helpers/notice.js
+++ Application/routes/helpers/notice.js
@@ -19,6 +19,21 @@ module.exports = {
       message = '現在解約手続き中です。'
       description1 = '解約手続き完了後、再度ご利用開始いただけます。'
       description2 = null
+    } else if (messageStatus === 'registerprocedure') {
+      // 解約手続き中画面を表示する
+      message = '現在利用登録手続き中です。'
+      description1 = '利用登録完了後、本機能はご利用可能となります。'
+      description2 = null
+    } else if (messageStatus === 'changeprocedure') {
+      // 解約手続き中画面を表示する
+      message = '現在契約情報変更手続き中です。'
+      description1 = '契約情報変更完了後、本機能は再度ご利用可能となります。'
+      description2 = null
+    } else if (messageStatus === 'generaluser') {
+      // 解約手続き中画面を表示する
+      message = '本機能はご利用いただけません。'
+      description1 = 'アカウント管理者権限のあるユーザで再度操作をお試しください。'
+      description2 = null
     } else {
       return next(errorHelper.create(400))
     }
