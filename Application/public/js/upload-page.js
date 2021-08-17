8e5e98593fd572bc49e650f979518f1c37e4cf6c
hong <hong@eclnttcom000670.onmicrosoft.com>
2021-08-17 18:05:59 +0900

    手動テストエラー対応（ファイルサイズ）

--- Application/public/js/upload-page.js
+++ Application/public/js/upload-page.js
@@ -1,62 +1,69 @@
-const fileUpload = document.getElementById('file-upload')
-const startUploadBtn = document.getElementById('start-upload-btn')
 const modal = document.getElementById('upload-progress-modal')
 const uploader = document.getElementsByClassName('uploader').item(0)
 let fileReader = null
 let targetFile = null
 
+const $ = (_selector) => {
+  const selectorDelimeter = _selector.substr(0, 1)
+  const selector = _selector.substr(1, _selector.length)
+
+  switch (selectorDelimeter) {
+    case '#':
+      return document.getElementById(selector)
+    case '.':
+      return document.getElementsByClassName(selector)
+  }
+}
+
 // 「アップロード開始」ボタンの活性化のスイッチ
-fileUpload.addEventListener('change', function (e) {
+$('#file-upload').addEventListener('change', function (e) {
   targetFile = this.files.item(0)
   if (targetFile !== null) {
     fileReader = new FileReader()
     fileReader.readAsBinaryString(targetFile)
     fileReader.onload = function () {
-      if (btoa(fileReader.result).length > 6826) {
+      if (btoa(fileReader.result).length > 6826000) {
         alert('ファイルサイズが5MB超えています。\nCSVファイルを確認後もう一度アップロードしてください。')
+        $('#start-upload-btn').setAttribute('Disabled', 'Disabled')
       } else {
-        startUploadBtn.removeAttribute('Disabled')
+        $('#start-upload-btn').removeAttribute('Disabled')
       }
     }
-  } else {
-    startUploadBtn.setAttribute('Disabled', 'Disabled')
   }
 })
 
 // CSVファイルをサーバーへアップロードする関数
-startUploadBtn.onclick = (() => {
-  return () => {
-    if (startUploadBtn.getAttribute('Disabled') || fileReader === null) {
-      return null
+$('#start-upload-btn').addEventListener('click', () => {
+  if ($('#start-upload-btn').getAttribute('Disabled') || fileReader === null) {
+    return null
+  } else {
+    modal.classList.add('is-active')
+    const sendData = {
+      filename: null,
+      fileData: null
+    }
+    if (fileReader.readyState !== 2) {
+      return
     } else {
-      modal.classList.add('is-active')
-      const sendData = {
-        filename: null,
-        fileData: null
-      }
-      if (fileReader.readyState !== 2) {
-        return
-      } else {
-        sendData.filename = targetFile.name
-        sendData.fileData = btoa(fileReader.result)
-        fileReader = null
-      }
-      const sender = new XMLHttpRequest()
-      sender.open('POST', uploader.action, true)
-      sender.setRequestHeader('Content-Type', 'application/json')
-      sender.onreadystatechange = () => {
-        if (sender.readyState === sender.DONE) {
-          if (sender.status === 200) {
-            modal.classList.remove('is-active')
-            startUploadBtn.setAttribute('Disabled', 'Disabled')
-            alert(sender.responseText)
-          } else {
-            modal.classList.remove('is-active')
-            startUploadBtn.setAttribute('Disabled', 'Disabled')
-          }
+      sendData.filename = targetFile.name
+      sendData.fileData = btoa(fileReader.result)
+      fileReader = null
+    }
+    const sender = new XMLHttpRequest()
+    sender.open('POST', uploader.action, true)
+    sender.setRequestHeader('Content-Type', 'application/json')
+    sender.onreadystatechange = () => {
+      if (sender.readyState === sender.DONE) {
+        if (sender.status === 200) {
+          modal.classList.remove('is-active')
+          $('#start-upload-btn').setAttribute('Disabled', 'Disabled')
+          alert(sender.responseText)
+        } else {
+          modal.classList.remove('is-active')
+          $('#start-upload-btn').setAttribute('Disabled', 'Disabled')
         }
       }
-      sender.send(JSON.stringify(sendData))
     }
+    sender.send(JSON.stringify(sendData))
   }
-})()
+})
