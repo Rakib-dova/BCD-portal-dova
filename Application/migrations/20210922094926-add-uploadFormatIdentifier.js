f0f17d66e86f85269a636da24887f4a0e584b662
jinho.kim <jinho.kim@eclnttcom000670.onmicrosoft.com>
2021-09-22 19:18:17 +0900

    ST#1619 【実装】DB作成

---
+++ Application/migrations/20210922094926-add-uploadFormatIdentifier.js
@@ -0,0 +1,21 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addConstraint('UploadFormatIdentifier', {
+      fields: ['uploadFormatId'],
+      type: 'foreign key',
+      name: 'fk_uploadFormatIdentifier_uploadFormat',
+      references: {
+        table: 'UploadFormat',
+        field: 'uploadFormatId'
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade'
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeConstraint('UploadFormatIdentifier', 'fk_uploadFormatIdentifier_uploadFormat')
+  }
+}
