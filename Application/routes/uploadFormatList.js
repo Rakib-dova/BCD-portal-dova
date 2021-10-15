c02d30a5d7b359fdc1b613ab22654b4324563bf7
root <root@gs042.cse_hn_domain.cseltd.co.jp>
2021-10-15 13:05:08 +0900

    [PBI1476][実装]フォーマット一覧画面作成

---
+++ Application/routes/uploadFormatList.js
@@ -0,0 +1,77 @@
+'use strict'
+const express = require('express')
+const router = express.Router()
+const helper = require('./helpers/middleware')
+const errorHelper = require('./helpers/error')
+const noticeHelper = require('./helpers/notice')
+const userController = require('../controllers/userController.js')
+const contractController = require('../controllers/contractController.js')
+const uploadFormatListController = require('../controllers/uploadFormatListController.js')
+const logger = require('../lib/logger')
+const validate = require('../lib/validate')
+const constantsDefine = require('../constants')
+
+const bodyParser = require('body-parser')
+router.use(
+  bodyParser.json({
+    type: 'application/json',
+    limit: '6826KB'
+  })
+)
+
+const cbGetIndex = async (req, res, next) => {
+  logger.info(constantsDefine.logMessage.INF000 + 'cbGetIndex')
+  // 認証情報取得処理
+  if (!req.session || !req.user?.userId) {
+    return next(errorHelper.create(500))
+  }
+
+  // DBからuserデータ取得
+  const user = await userController.findOne(req.user.userId)
+  // データベースエラーは、エラーオブジェクトが返る
+  // user未登録の場合もエラーを上げる
+  if (user instanceof Error || user === null) return next(errorHelper.create(500))
+
+  // TX依頼後に改修、ユーザステイタスが0以外の場合、「404」エラーとする not 403
+  if (user.dataValues?.userStatus !== 0) return next(errorHelper.create(404))
+
+  // DBから契約情報取得
+  const contract = await contractController.findOne(req.user.tenantId)
+  // データベースエラーは、エラーオブジェクトが返る
+  // 契約情報未登録の場合もエラーを上げる
+  if (contract instanceof Error || contract === null) return next(errorHelper.create(500))
+
+  req.session.userContext = 'LoggedIn'
+
+  // ユーザ権限を取得
+  req.session.userRole = user.dataValues?.userRole
+  const deleteFlag = contract.dataValues.deleteFlag
+  const contractStatus = contract.dataValues.contractStatus
+  const checkContractStatus = helper.checkContractStatus
+
+  if (checkContractStatus === null || checkContractStatus === 999) {
+    return next(errorHelper.create(500))
+  }
+
+  if (!validate.isStatusForCancel(contractStatus, deleteFlag)) {
+    return next(noticeHelper.create('cancelprocedure'))
+  }
+
+  // アップロードフォーマット取得
+  const uploadFormatListArr = await uploadFormatListController.getFormatList(req.user.tenantId)
+
+  if (uploadFormatListArr instanceof Error || uploadFormatListArr === null) return next(errorHelper.create(500))
+
+  // ユーザ権限も画面に送る
+  res.render('uploadFormatList', {
+    uploadFormatListArr: uploadFormatListArr
+  })
+  logger.info(constantsDefine.logMessage.INF001 + 'cbGetIndex')
+}
+
+router.get('/', helper.isAuthenticated, cbGetIndex)
+
+module.exports = {
+  router: router,
+  cbGetIndex: cbGetIndex
+}
