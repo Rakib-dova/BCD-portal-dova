63f1849c7fc15d70c35710389eba8d3a2b01f307
dongwoo kang <kang@eclnttcom000670.onmicrosoft.com>
2021-11-12 18:57:37 +0900

    【不具合対応】ST#2409_商用環境で発生した不具合対応（アップロードフォーマット一覧、CSVアップロードの表示）

--- Application/lib/bconCsvNoHeader.js
+++ Application/lib/bconCsvNoHeader.js
@@ -1123,8 +1123,9 @@ class bconCsvNoHeader {
       if (identifier.extensionType === '0') {
         taxidentifier.map((tax) => {
           if (tax === identifier.defaultExtension) {
-            bconCsvTaxUser[identifier.uploadFormatExtension] = bconCsvTaxUser[tax]
+            const taxValue = bconCsvTaxUser[tax]
             delete bconCsvTaxUser[tax]
+            bconCsvTaxUser[identifier.uploadFormatExtension] = taxValue
           }
         })
       }
@@ -1140,8 +1141,9 @@ class bconCsvNoHeader {
       if (identifier.extensionType === '1') {
         unitidentifier.map((unit) => {
           if (unit === identifier.defaultExtension) {
-            bconCsvUnitUser[identifier.uploadFormatExtension] = bconCsvUnitUser[unit]
+            const unitValue = bconCsvUnitUser[unit]
             delete bconCsvUnitUser[unit]
+            bconCsvUnitUser[identifier.uploadFormatExtension] = unitValue
           }
         })
       }
