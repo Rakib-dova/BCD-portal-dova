34684c937ed4f2d2e1de7dc8f5f2f399b77b5f4d
pro_hong <pro_hong@eclnttcom100009.onmicrosoft.com>
2021-11-25 17:06:06 +0900

    【PBI1826】【製造】ST#2529_UTコード作成

--- Tests/mockDB/Contracts_Table.js
+++ Tests/mockDB/Contracts_Table.js
@@ -11,6 +11,7 @@
 // canceledAt
 
 module.exports = [
+  // 契約DB値：契約中：0
   {
     contractId: 'a31fe56d-6ea1-49a2-95f9-200e370984f8',
     tenantId: '5778c070-5dd3-42db-aaa8-848424fb80f9',
@@ -27,6 +28,167 @@ module.exports = [
       numberN: '123456789',
       contractStatus: '00',
       deleteFlag: '0',
+      createdAt: '2021-10-26 10:40:27.3680000 +00:00',
+      updatedAt: '2021-11-05 04:03:57.1680000 +00:00',
+      contractedAt: null,
+      canceledAt: null
+    }
+  },
+  // 契約DB値：登録申込中：1
+  {
+    contractId: '4df15ca1-ead9-45c6-b546-3e52d06007e9',
+    tenantId: 'b8537573-0f8d-4272-857c-a7b19dd76fa1',
+    numberN: 'AA010',
+    contractStatus: '10',
+    deleteFlag: false,
+    createdAt: '2021-10-26 10:40:27.3680000 +00:00',
+    updatedAt: '2021-11-05 04:03:57.1680000 +00:00',
+    contractedAt: null,
+    canceledAt: null,
+    dataValues: {
+      contractId: '4df15ca1-ead9-45c6-b546-3e52d06007e9',
+      tenantId: 'b8537573-0f8d-4272-857c-a7b19dd76fa1',
+      numberN: 'AA010',
+      contractStatus: '10',
+      deleteFlag: false,
+      createdAt: '2021-10-26 10:40:27.3680000 +00:00',
+      updatedAt: '2021-11-05 04:03:57.1680000 +00:00',
+      contractedAt: null,
+      canceledAt: null
+    }
+  },
+  // 契約DB値：登録受取中：2
+  {
+    contractId: '92b2ecb9-6def-4b51-bee8-e8a8d0c651fc',
+    tenantId: '86da619c-17b8-4b43-bcb6-d05f3994608f',
+    numberN: 'AA010',
+    contractStatus: '11',
+    deleteFlag: false,
+    createdAt: '2021-10-26 10:40:27.3680000 +00:00',
+    updatedAt: '2021-11-05 04:03:57.1680000 +00:00',
+    contractedAt: null,
+    canceledAt: null,
+    dataValues: {
+      contractId: '4df15ca1-ead9-45c6-b546-3e52d06007e9',
+      tenantId: 'b8537573-0f8d-4272-857c-a7b19dd76fa1',
+      numberN: 'AA010',
+      contractStatus: '11',
+      deleteFlag: false,
+      createdAt: '2021-10-26 10:40:27.3680000 +00:00',
+      updatedAt: '2021-11-05 04:03:57.1680000 +00:00',
+      contractedAt: null,
+      canceledAt: null
+    }
+  },
+  // 契約DB値：変更申込中：3
+  {
+    contractId: '1df09ef6-aab8-4baf-95ea-89c4a99604fc',
+    tenantId: 'c02659e2-2808-465f-956e-48fac354c390',
+    numberN: 'AA040',
+    contractStatus: '40',
+    deleteFlag: false,
+    createdAt: '2021-10-26 10:40:27.3680000 +00:00',
+    updatedAt: '2021-11-05 04:03:57.1680000 +00:00',
+    contractedAt: null,
+    canceledAt: null,
+    dataValues: {
+      contractId: '1df09ef6-aab8-4baf-95ea-89c4a99604fc',
+      tenantId: 'c02659e2-2808-465f-956e-48fac354c390',
+      numberN: 'AA010',
+      contractStatus: '40',
+      deleteFlag: false,
+      createdAt: '2021-10-26 10:40:27.3680000 +00:00',
+      updatedAt: '2021-11-05 04:03:57.1680000 +00:00',
+      contractedAt: null,
+      canceledAt: null
+    }
+  },
+  // 契約DB値：変更受取中：4
+  {
+    contractId: '38c7965d-41f4-4ac8-86ac-e3195a03670c',
+    tenantId: '29006b9a-cf04-4849-8bac-e73125a8933c',
+    numberN: 'AA040',
+    contractStatus: '41',
+    deleteFlag: false,
+    createdAt: '2021-10-26 10:40:27.3680000 +00:00',
+    updatedAt: '2021-11-05 04:03:57.1680000 +00:00',
+    contractedAt: null,
+    canceledAt: null,
+    dataValues: {
+      contractId: '38c7965d-41f4-4ac8-86ac-e3195a03670c',
+      tenantId: '29006b9a-cf04-4849-8bac-e73125a8933c',
+      numberN: 'AA010',
+      contractStatus: '41',
+      deleteFlag: false,
+      createdAt: '2021-10-26 10:40:27.3680000 +00:00',
+      updatedAt: '2021-11-05 04:03:57.1680000 +00:00',
+      contractedAt: null,
+      canceledAt: null
+    }
+  },
+  // 契約DB値：解約申込中：5
+  {
+    contractId: '04792ed4-b99c-46ba-a737-54f2ec695a17',
+    tenantId: '57643b5d-4908-4ee6-89f4-70e0b3d7c9bb',
+    numberN: 'AA030',
+    contractStatus: '30',
+    deleteFlag: false,
+    createdAt: '2021-10-26 10:40:27.3680000 +00:00',
+    updatedAt: '2021-11-05 04:03:57.1680000 +00:00',
+    contractedAt: null,
+    canceledAt: null,
+    dataValues: {
+      contractId: '04792ed4-b99c-46ba-a737-54f2ec695a17',
+      tenantId: '57643b5d-4908-4ee6-89f4-70e0b3d7c9bb',
+      numberN: 'AA030',
+      contractStatus: '30',
+      deleteFlag: false,
+      createdAt: '2021-10-26 10:40:27.3680000 +00:00',
+      updatedAt: '2021-11-05 04:03:57.1680000 +00:00',
+      contractedAt: null,
+      canceledAt: null
+    }
+  },
+  // 契約DB値：解約受取中：6
+  {
+    contractId: 'cb6569de-bc74-42ec-baa3-4dea1c943972',
+    tenantId: '86fe6717-63c4-4e82-89ac-6fc61a15ecd1',
+    numberN: 'AA031',
+    contractStatus: '31',
+    deleteFlag: false,
+    createdAt: '2021-10-26 10:40:27.3680000 +00:00',
+    updatedAt: '2021-11-05 04:03:57.1680000 +00:00',
+    contractedAt: null,
+    canceledAt: null,
+    dataValues: {
+      contractId: 'cb6569de-bc74-42ec-baa3-4dea1c943972',
+      tenantId: '86fe6717-63c4-4e82-89ac-6fc61a15ecd1',
+      numberN: 'AA031',
+      contractStatus: '31',
+      deleteFlag: false,
+      createdAt: '2021-10-26 10:40:27.3680000 +00:00',
+      updatedAt: '2021-11-05 04:03:57.1680000 +00:00',
+      contractedAt: null,
+      canceledAt: null
+    }
+  },
+  // 契約DB値：解約完了：7
+  {
+    contractId: 'da73dbea-bfa6-450f-ba07-96577c93d267',
+    tenantId: '311ef4a9-e79a-4abb-84c9-30c23852f97e',
+    numberN: '',
+    contractStatus: null,
+    deleteFlag: true,
+    createdAt: '2021-10-26 10:40:27.3680000 +00:00',
+    updatedAt: '2021-11-05 04:03:57.1680000 +00:00',
+    contractedAt: null,
+    canceledAt: null,
+    dataValues: {
+      contractId: 'da73dbea-bfa6-450f-ba07-96577c93d267',
+      tenantId: '311ef4a9-e79a-4abb-84c9-30c23852f97e',
+      numberN: '',
+      contractStatus: null,
+      deleteFlag: true,
       createdAt: '2021-10-26 10:40:27.3680000 +00:00',
       updatedAt: '2021-11-05 04:03:57.1680000 +00:00',
       contractedAt: null,
