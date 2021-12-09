bb15fe4e71a7e014f8c021906da058d7915fe92f
erina-kon <kon@actec2.omicrosoft.com>
2021-12-09 17:16:48 +0900

    ST#2873 Cookieのセット

--- Application/routes/index.js
+++ Application/routes/index.js
@@ -4,28 +4,29 @@ const express = require('express')
 const router = express.Router()
 
 const cbGetIndex = (req, res, next) => {
-  // 将来的にポータルがトレードシフトから切り離される場合に備えて、indexを確保
-  // 現在はポータルへのリダイレクトのみとする
-  res.redirect(303, '/auth')
-}
+  const test = req.query.test
 
-const cbGetRegister = (req, res, next) => {
-  // アカウント作成 遷移
-  res.redirect(303, 'https://sandbox.tradeshift.com/register')
-}
-
-const cbGetLogin = (req, res, next) => {
-  // ログイン 遷移
-  res.redirect(303, 'https://sandbox.tradeshift.com/?currentScreen=0')
+  if (test === 'sinup_dxstore') {
+    // dxストアから遷移した場合、Cookieをセット
+    res.setHeader('Set-Cookie', 'referer=dxstore;')
+    // アカウント登録ページに遷移
+    res.redirect(303, 'https://sandbox.tradeshift.com/register')
+  } else if (test === 'sinup') {
+    // アカウント登録ページに遷移
+    res.redirect(303, 'https://sandbox.tradeshift.com/register')
+  } else if (test === 'login') {
+    // ログインページに遷移
+    res.redirect(303, 'https://sandbox.tradeshift.com/?currentScreen=0')
+  } else {
+    // 将来的にポータルがトレードシフトから切り離される場合に備えて、indexを確保
+    // 現在はポータルへのリダイレクトのみとする
+    res.redirect(303, '/auth')
+  }
 }
 
 router.get('/', cbGetIndex)
-router.get('/gtm_sinup', cbGetRegister)
-router.get('/gtm_login', cbGetLogin)
 
 module.exports = {
   router: router,
-  cbGetIndex: cbGetIndex,
-  cbGetRegister: cbGetRegister,
-  cbGetLogin: cbGetLogin
+  cbGetIndex: cbGetIndex
 }
