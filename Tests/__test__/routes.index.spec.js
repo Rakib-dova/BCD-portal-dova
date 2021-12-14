6aaecbd6d0d4d528e96c6c4d4e197ff95b56200d
erina-kon <kon@actec2.omicrosoft.com>
2021-12-14 12:10:19 +0900

    ST#2859 テストコードの修正

--- Tests/__test__/routes.index.spec.js
+++ Tests/__test__/routes.index.spec.js
@@ -36,5 +36,57 @@ describe('indexのテスト', () => {
       expect(response.redirect).toHaveBeenCalledWith(303, '/auth')
       expect(response.getHeader('Location')).toEqual('/auth')
     })
+
+    test('アカウント登録ページに遷移(dx_store経由)', async () => {
+      request.query.transition = 'sinup'
+      request.query.dxstoreFlg = 'true'
+
+      await index.cbGetIndex(request, response, next)
+      expect(response.redirect).toHaveBeenCalled()
+      expect(response.redirect).toHaveBeenCalledWith(303, 'https://sandbox.tradeshift.com/register')
+      expect(response.getHeader('Location')).toEqual('https://sandbox.tradeshift.com/register')
+
+      expect(response.cookie).toHaveBeenCalledWith('CustomReferrer', 'dxstore', {
+        secure: true,
+        httpOnly: false,
+        sameSite: 'none',
+        maxAge: 60000
+      })
+    })
+
+    test('アカウント登録ページに遷移', async () => {
+      request.query.transition = 'sinup'
+
+      await index.cbGetIndex(request, response, next)
+      expect(response.redirect).toHaveBeenCalled()
+      expect(response.redirect).toHaveBeenCalledWith(303, 'https://sandbox.tradeshift.com/register')
+      expect(response.getHeader('Location')).toEqual('https://sandbox.tradeshift.com/register')
+    })
+
+    test('ログインページページに遷移(dx_store経由)', async () => {
+      request.query.transition = 'login'
+      request.query.dxstoreFlg = 'true'
+
+      await index.cbGetIndex(request, response, next)
+      expect(response.redirect).toHaveBeenCalled()
+      expect(response.redirect).toHaveBeenCalledWith(303, 'https://sandbox.tradeshift.com/?currentScreen=0')
+      expect(response.getHeader('Location')).toEqual('https://sandbox.tradeshift.com/?currentScreen=0')
+
+      expect(response.cookie).toHaveBeenCalledWith('CustomReferrer', 'dxstore', {
+        secure: true,
+        httpOnly: false,
+        sameSite: 'none',
+        maxAge: 60000
+      })
+    })
+
+    test('ログインページページに遷移', async () => {
+      request.query.transition = 'login'
+
+      await index.cbGetIndex(request, response, next)
+      expect(response.redirect).toHaveBeenCalled()
+      expect(response.redirect).toHaveBeenCalledWith(303, 'https://sandbox.tradeshift.com/?currentScreen=0')
+      expect(response.getHeader('Location')).toEqual('https://sandbox.tradeshift.com/?currentScreen=0')
+    })
   })
 })
