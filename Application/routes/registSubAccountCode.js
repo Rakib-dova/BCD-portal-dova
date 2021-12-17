b83b4e6f1555dfe98a5524ed5ea2bbc6c7bc543d
root <root@gs042.cse_hn_domain.cseltd.co.jp>
2021-12-17 19:19:28 +0900

    [PBI#1830]【実装】補助科目の登録処理

--- Application/routes/registSubAccountCode.js
+++ Application/routes/registSubAccountCode.js
@@ -11,6 +11,7 @@ const contractController = require('../controllers/contractController.js')
 const logger = require('../lib/logger')
 const constantsDefine = require('../constants')
 const accountCodeController = require('../controllers/accountCodeController')
+const subAccountCodeController = require('../controllers/subAccountCodeController.js')
 const bodyParser = require('body-parser')
 router.use(
   bodyParser.json({
@@ -132,11 +133,81 @@ const cbPostGetAccountCode = async (req, res, next) => {
   logger.info(constantsDefine.logMessage.INF001 + 'cbPostGetAccountCode')
 }
 
+const cbPostIndex = async (req, res, next) => {
+  logger.info(constantsDefine.logMessage.INF000 + 'cbPostIndex')
+
+  // 認証情報取得処理
+  if (!req.session || !req.user?.userId) return next(errorHelper.create(500))
+
+  // DBからuserデータ取得
+  const user = await userController.findOne(req.user.userId)
+  // データベースエラーは、エラーオブジェクトが返る
+  // user未登録の場合もエラーを上げる
+  if (user instanceof Error || user === null) return next(errorHelper.create(500))
+
+  // TX依頼後に改修、ユーザステイタスが0以外の場合、「404」エラーとする not 403
+  if (user.dataValues?.userStatus !== 0) return next(errorHelper.create(404))
+  if (req.session?.userContext !== 'LoggedIn') return next(errorHelper.create(400))
+
+  // DBから契約情報取得
+  const contract = await contractController.findOne(req.user.tenantId)
+  // データベースエラーは、エラーオブジェクトが返る
+  // 契約情報未登録の場合もエラーを上げる
+  if (contract instanceof Error || contract === null) return next(errorHelper.create(500))
+
+  // ユーザ権限を取得
+  req.session.userRole = user.dataValues?.userRole
+  const deleteFlag = contract.dataValues.deleteFlag
+  const contractStatus = contract.dataValues.contractStatus
+  const checkContractStatus = await helper.checkContractStatus(req.user.tenantId)
+
+  if (checkContractStatus === null || checkContractStatus === 999) return next(errorHelper.create(500))
+
+  if (!validate.isStatusForCancel(contractStatus, deleteFlag)) return next(noticeHelper.create('cancelprocedure'))
+
+  // 勘定科目accountCodeId
+  const accountCode = req.body.setAccountCodeInputId
+  // 補助科目
+  const subAccountCodeId = req.body.setSubAccountCodeInputId
+  // 補助科目名
+  const subAccountCodeName = req.body.setSubAccountCodeNameInputId
+  const values = {
+    accountCode: accountCode,
+    subjectCode: subAccountCodeId,
+    subjectName: subAccountCodeName
+  }
+
+  // 補助科目をDBに保存する。
+  // 結果：0 正常登録、1 中腹登録、-1 登録失敗、Error DBエラー発生
+  const result = await subAccountCodeController.insert(contract, values)
+  if (result instanceof Error) return next(errorHelper.create(500))
+  // 結果確認
+  switch (result) {
+    case 0:
+      // 正常に登録ができた場合
+      req.flash('info', '補助科目を登録しました。')
+      res.redirect('/portal')
+      break
+    case 1:
+      req.flash('noti', ['補助科目登録', 'すでに登録されている値です。'])
+      res.redirect('/registSubAccountCode')
+      break
+    case -1:
+      req.flash('noti', ['補助科目登録', '補助科目登録に失敗しました。'])
+      res.redirect('/registSubAccountCode')
+      break
+  }
+
+  logger.info(constantsDefine.logMessage.INF001 + 'cbPostIndex')
+}
+
 router.get('/', helper.isAuthenticated, cbGetIndex)
 router.post('/getAccountCode', helper.isAuthenticated, cbPostGetAccountCode)
+router.post('/', helper.isAuthenticated, cbPostIndex)
 
 module.exports = {
   router: router,
   cbGetIndex: cbGetIndex,
-  cbPostGetAccountCode: cbPostGetAccountCode
+  cbPostGetAccountCode: cbPostGetAccountCode,
+  cbPostIndex: cbPostIndex
 }
