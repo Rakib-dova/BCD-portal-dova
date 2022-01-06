0b5a1c8c0a49cf665313f6fb3d9dd36bc9b0e45c
pro_hong <pro_hong@eclnttcom100009.onmicrosoft.com>
2022-01-06 10:30:26 +0900

    【PBI2173】【製造】ST#3187_画面作成共通ファイル作成

---
+++ Application/routes/inbox.js
@@ -0,0 +1,70 @@
+'use strict'
+const express = require('express')
+const router = express.Router()
+const helper = require('./helpers/middleware')
+const errorHelper = require('./helpers/error')
+const noticeHelper = require('./helpers/notice')
+const userController = require('../controllers/userController.js')
+const contractController = require('../controllers/contractController.js')
+const logger = require('../lib/logger')
+const validate = require('../lib/validate')
+const constantsDefine = require('../constants')
+const inboxController = require('../controllers/inboxController')
+
+const cbGetIndex = async (req, res, next) => {
+  logger.info(constantsDefine.logMessage.INF000 + 'cbGetIndex')
+  // 認証情報取得処理
+  if (!req.session || !req.user?.userId) {
+    return next(errorHelper.create(500))
+  }
+
+  // DBからuserデータ取得
+  const user = await userController.findOne(req.user.userId)
+  // データベースエラーは、エラーオブジェクトが返る
+  // user未登録の場合もエラーを上げる
+  if (user instanceof Error || user === null) return next(errorHelper.create(500))
+
+  // TX依頼後に改修、ユーザステイタスが0以外の場合、「404」エラーとする not 403
+  if (user.dataValues?.userStatus !== 0) return next(errorHelper.create(404))
+
+  // DBから契約情報取得
+  const contract = await contractController.findOne(req.user.tenantId)
+  // データベースエラーは、エラーオブジェクトが返る
+  // 契約情報未登録の場合もエラーを上げる
+  if (contract instanceof Error || contract === null) return next(errorHelper.create(500))
+
+  req.session.userContext = 'LoggedIn'
+
+  // ユーザ権限を取得
+  req.session.userRole = user.dataValues?.userRole
+  const deleteFlag = contract.dataValues.deleteFlag
+  const contractStatus = contract.dataValues.contractStatus
+  const checkContractStatus = await helper.checkContractStatus(req.user.tenantId)
+
+  if (checkContractStatus === null || checkContractStatus === 999) {
+    return next(errorHelper.create(500))
+  }
+
+  if (!validate.isStatusForCancel(contractStatus, deleteFlag)) {
+    return next(noticeHelper.create('cancelprocedure'))
+  }
+
+  // ページ取得
+  const accessToken = req.user.accessToken
+  const refreshToken = req.user.refreshToken
+  const pageId = ~~req.params.page
+  const tenantId = user.tenantId
+  // const result = await inboxController.getInbox(accessToken, refreshToken, pageId, tenantId)
+
+  // 受領した請求書一覧レンダリング
+  res.render('inbox', {})
+
+  logger.info(constantsDefine.logMessage.INF001 + 'cbGetIndex')
+}
+
+router.get('/:invoiceId', helper.isAuthenticated, cbGetIndex)
+
+module.exports = {
+  router: router,
+  cbGetIndex: cbGetIndex
+}
