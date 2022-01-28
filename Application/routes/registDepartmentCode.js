d7c30fc028a9594f285337e959d62dfb35e65d62
root <root@gs042.cse_hn_domain.cseltd.co.jp>
2022-01-28 13:13:11 +0900

    [PBI#1833] ST#3539【実装】部門コードの登録画面作成

---
+++ Application/routes/registDepartmentCode.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+const helper = require('./helpers/middleware')
+const validate = require('../lib/validate')
+const errorHelper = require('./helpers/error')
+const noticeHelper = require('./helpers/notice')
+const userController = require('../controllers/userController.js')
+const contractController = require('../controllers/contractController.js')
+const logger = require('../lib/logger')
+const constantsDefine = require('../constants')
+
+const cbGetRegistDepartmentCode = async (req, res, next) => {
+  logger.info(constantsDefine.logMessage.INF000 + 'cbGetRegistDepartmentCode')
+
+  // 認証情報取得処理
+  if (!req.session || !req.user?.userId) return next(errorHelper.create(500))
+
+  // DBからuserデータ取得
+  const user = await userController.findOne(req.user.userId)
+  // データベースエラーは、エラーオブジェクトが返る
+  // user未登録の場合もエラーを上げる
+  if (user instanceof Error || user === null) return next(errorHelper.create(500))
+
+  // TX依頼後に改修、ユーザステイタスが0以外の場合、「404」エラーとする not 403
+  if (user.dataValues?.userStatus !== 0) return next(errorHelper.create(404))
+  if (req.session?.userContext !== 'LoggedIn') return next(errorHelper.create(400))
+
+  // DBから契約情報取得
+  const contract = await contractController.findOne(req.user.tenantId)
+  // データベースエラーは、エラーオブジェクトが返る
+  // 契約情報未登録の場合もエラーを上げる
+  if (contract instanceof Error || contract === null) return next(errorHelper.create(500))
+
+  // ユーザ権限を取得
+  req.session.userRole = user.dataValues?.userRole
+  const deleteFlag = contract.dataValues.deleteFlag
+  const contractStatus = contract.dataValues.contractStatus
+  const checkContractStatus = await helper.checkContractStatus(req.user.tenantId)
+
+  if (checkContractStatus === null || checkContractStatus === 999) return next(errorHelper.create(500))
+
+  if (!validate.isStatusForCancel(contractStatus, deleteFlag)) return next(noticeHelper.create('cancelprocedure'))
+
+  res.render('registDepartmentCode', {
+    codeName: '部門データ',
+    codeLabel: '部門コード',
+    codeNameLabel: '部門名',
+    requiredTagCode: 'departmentCodeTagRequired',
+    requiredTagName: 'departmentCodeNameRequired',
+    idForCodeInput: 'setDepartmentCodeInputId',
+    idForNameInput: 'setDepartmentCodeNameInputId',
+    modalTitle: '部門データ設定確認',
+    backUrl: '/',
+    logTitle: '部門データ登録',
+    logTitleEng: 'REGIST DEPARTMENT CODE',
+    isRegistDepartmentCode: true,
+    pTagForcheckInput1: 'checksetDepartmentCodeInputId',
+    pTagForcheckInput2: 'checksetDepartmentCodeNameInputId',
+    checkModalLabel1: '部門コード',
+    checkModalLabel2: '部門名'
+  })
+
+  logger.info(constantsDefine.logMessage.INF001 + 'cbGetRegistDepartmentCode')
+}
+
+router.get('/', helper.isAuthenticated, cbGetRegistDepartmentCode)
+
+module.exports = {
+  router: router,
+  cbGetRegistDepartmentCode: cbGetRegistDepartmentCode
+}
