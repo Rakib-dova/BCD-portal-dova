121cf3de951f47b2cac31b16c9511c8626690627
Hiroya Murata <hiroya@use-ebisu.co.jp>
2022-02-03 13:50:53 +0900

    モジュールパスを修正

--- Application/obc/routes/helpers/middleware.js
+++ Application/obc/routes/helpers/middleware.js
@@ -1,5 +1,5 @@
 'use strict'
-const portal = require('../../../../routes/helpers/middleware')
+const portal = require('../../../routes/helpers/middleware')
 
 const isBugyoAuthenticated = async (req, res, next) => {
   const bugyo = req && req.session && req.session.bugyo
