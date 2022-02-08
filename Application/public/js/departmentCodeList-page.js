3438daa33d08de861aa09aac6c9debf6187a582d
root <root@gs042.cse_hn_domain.cseltd.co.jp>
2022-02-08 18:22:30 +0900

    [PBI#731] ST#3711【リファクタリング】「＄」関数共通化(PB1837)

--- Application/public/js/departmentCodeList-page.js
+++ Application/public/js/departmentCodeList-page.js
@@ -1,22 +1,24 @@
-// document.getElementById、document.getElementsByClassName省略
-const $ = function (tagObjName) {
-  const classNamePattern = '\\.+[a-zA-Z0-9]'
-  const idNamePatten = '\\#+[a-zA-Z0-9]'
-  const classNameReg = new RegExp(classNamePattern)
-  const idNameReg = new RegExp(idNamePatten)
-  let selectors
+import { $ } from '../module/getElements.js'
 
-  if (classNameReg.test(tagObjName)) {
-    selectors = document.querySelectorAll(tagObjName)
-  } else if (idNameReg.test(tagObjName)) {
-    selectors = document.querySelectorAll(tagObjName)[0]
-  } else {
-    return null
-  }
-  return Object.assign(selectors, Array.prototype, (type, event) => {
-    document.addEventListener(type, event)
-  })
-}
+// document.getElementById、document.getElementsByClassName省略
+// const $ = function (tagObjName) {
+//   const classNamePattern = '\\.+[a-zA-Z0-9]'
+//   const idNamePatten = '\\#+[a-zA-Z0-9]'
+//   const classNameReg = new RegExp(classNamePattern)
+//   const idNameReg = new RegExp(idNamePatten)
+//   let selectors
+
+//   if (classNameReg.test(tagObjName)) {
+//     selectors = document.querySelectorAll(tagObjName)
+//   } else if (idNameReg.test(tagObjName)) {
+//     selectors = document.querySelectorAll(tagObjName)[0]
+//   } else {
+//     return null
+//   }
+//   return Object.assign(selectors, Array.prototype, (type, event) => {
+//     document.addEventListener(type, event)
+//   })
+// }
 
 // UserAgentで判定し
 // IE以外は動的にスクリプトをロード
