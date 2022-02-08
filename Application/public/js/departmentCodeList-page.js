5ec7519730d03f8b95ab30e378e967771103b5e9
dongwoo kang <kang@eclnttcom000670.onmicrosoft.com>
2022-02-08 14:51:50 +0900

    【PBI1837】【製造】ST#3640_部門コード削除処理

--- Application/public/js/departmentCodeList-page.js
+++ Application/public/js/departmentCodeList-page.js
@@ -1,3 +1,23 @@
+// document.getElementById、document.getElementsByClassName省略
+const $ = function (tagObjName) {
+  const classNamePattern = '\\.+[a-zA-Z0-9]'
+  const idNamePatten = '\\#+[a-zA-Z0-9]'
+  const classNameReg = new RegExp(classNamePattern)
+  const idNameReg = new RegExp(idNamePatten)
+  let selectors
+
+  if (classNameReg.test(tagObjName)) {
+    selectors = document.querySelectorAll(tagObjName)
+  } else if (idNameReg.test(tagObjName)) {
+    selectors = document.querySelectorAll(tagObjName)[0]
+  } else {
+    return null
+  }
+  return Object.assign(selectors, Array.prototype, (type, event) => {
+    document.addEventListener(type, event)
+  })
+}
+
 // UserAgentで判定し
 // IE以外は動的にスクリプトをロード
 const ua = window.navigator.userAgent
@@ -54,6 +74,45 @@ Array.prototype.forEach.call(document.querySelectorAll('.checkChangeDepartmentCo
             break
           default:
             alert('システムエラーが発生しました。')
+            break
+        }
+      })
+  })
+})
+
+// 一覧から各レコードの削除ボタン押下
+Array.prototype.forEach.call(document.querySelectorAll('.deleteDepartmentCodeBtn'), (item) => {
+  item.addEventListener('click', () => {
+    const departmentCodeId = item.getAttribute('uuid')
+    $('#modalCodeDelBtn').setAttribute('uuid', departmentCodeId)
+  })
+})
+
+// Modalの削除ボタン押下
+Array.prototype.forEach.call(document.querySelectorAll('#modalCodeDelBtn'), (item) => {
+  item.addEventListener('click', function (e) {
+    const departmentCodeId = item.getAttribute('uuid')
+    const url = `/deleteDepartmentCode/${departmentCodeId}`
+    fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        // 削除失敗
+        switch (response.result) {
+          case 0:
+            alert('削除失敗しました。（システムエラー）')
+            break
+          case 1:
+            // ページ更新
+            location.reload()
+            break
+          case -1:
+            alert('既に削除されています。\n「OK」ボタンを押下し、画面内容を最新にします。')
+            location.reload()
             break
         }
       })
