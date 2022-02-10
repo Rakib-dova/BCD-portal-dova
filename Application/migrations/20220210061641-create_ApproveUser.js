a969c9a22059ee26312929769ee1a8db9ac46e66
root <root@gs042.cse_hn_domain.cseltd.co.jp>
2022-02-10 16:02:10 +0900

    [PBI#3550] ST#3723【実装】ルート登録に必要な複数ののテーブル作成（ローカル）

---
+++ Application/migrations/20220210061641-create_ApproveUser.js
@@ -0,0 +1,57 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('ApproveUser', {
+      approveUserId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      approveRouteId: {
+        allowNull: false,
+        type: Sequelize.UUID
+      },
+      approveUsers: {
+        allowNull: false,
+        type: Sequelize.UUID
+      },
+      prevApproveUser: {
+        allowNull: false,
+        type: Sequelize.UUID
+      },
+      nextApproveUser: {
+        allowNull: false,
+        type: Sequelize.UUID
+      },
+      lastApproveUserId: {
+        allowNull: false,
+        type: Sequelize.UUID
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('ApproveUser')
+  }
+}
