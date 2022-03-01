955330a2030d24c5045b40e4a7d0fd2eb5365e04
root <root@gs042.cse_hn_domain.cseltd.co.jp>
2022-03-01 22:41:00 +0900

    [PB#3580]【レビュー後修正】保存処理修正や画面Flash処理

--- Application/public/js/requestApproval-page.js
+++ Application/public/js/requestApproval-page.js
@@ -1,3 +1,16 @@
+// UserAgentで判定し
+// IE以外は動的にスクリプトをロード
+const ua = window.navigator.userAgent
+if (ua.indexOf('MSIE ') === -1 && ua.indexOf('Trident') === -1) {
+  const tag = document.createElement('script')
+  tag.type = 'module'
+  tag.src = '/js/loaded-portal-page.js'
+  document.getElementsByTagName('body')[0].appendChild(tag)
+} else {
+  // IEはクリップボードコピーが機能しないのでコピーボタンを削除
+  const elm = document.getElementById('copy-btn')
+  if (elm) elm.parentNode.removeChild(elm)
+}
 // document.getElementById、document.getElementsByClassName省略
 const $ = function (tagObjName) {
   const classNamePattern = '\\.+[a-zA-Z0-9]'
