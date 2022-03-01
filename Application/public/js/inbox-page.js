dd6f26b34bd097dd0a11d73d1d72cfec8efe75cf
jinho.kim <jinho.kim@eclnttcom000670.onmicrosoft.com>
2022-03-01 23:04:08 +0900

    【PBI3580】【レビュー後修正】ポップアップ表示漏れ

--- Application/public/js/inbox-page.js
+++ Application/public/js/inbox-page.js
@@ -19,6 +19,20 @@ const $ = function (tagObjName) {
   })
 }
 
+// UserAgentで判定し
+// IE以外は動的にスクリプトをロード
+const ua = window.navigator.userAgent
+if (ua.indexOf('MSIE ') === -1 && ua.indexOf('Trident') === -1) {
+  const tag = document.createElement('script')
+  tag.type = 'module'
+  tag.src = '/js/loaded-portal-page.js'
+  document.getElementsByTagName('body')[0].appendChild(tag)
+} else {
+  // IEはクリップボードコピーが機能しないのでコピーボタンを削除
+  const elm = document.getElementById('copy-btn')
+  if (elm) elm.parentNode.removeChild(elm)
+}
+
 // ローディング画面の初期化
 window.onload = function () {
   // 計上金額のボタンの機能設定
