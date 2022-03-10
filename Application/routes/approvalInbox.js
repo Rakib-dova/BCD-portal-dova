6049ba798d9556e72ae18e08624e8eb0ec89c626
jinho.kim <jinho.kim@eclnttcom000670.onmicrosoft.com>
2022-03-10 16:00:24 +0900

    【PBI3583】【実装】ST#4087_インテグレーションテストコード作成

--- Application/routes/approvalInbox.js
+++ Application/routes/approvalInbox.js
@@ -105,7 +105,7 @@ const cbGetIndex = async (req, res, next) => {
 
   res.render(presentation, {
     ...result,
-    title: '承認依頼',
+    title: '支払依頼',
     documentId: invoiceId,
     approveRoute: approveRoute,
     prevUser: prevUser
@@ -170,7 +170,7 @@ const cbPostApprove = async (req, res, next) => {
       break
     case -1:
       req.flash('noti', [
-        '承認依頼',
+        '支払依頼',
         `仕訳情報設定が完了できませんでした。<BR>※明細ID「${lineId}」の勘定科目「${accountCode}」は未登録勘定科目です。`,
         'SYSERR'
       ])
@@ -178,7 +178,7 @@ const cbPostApprove = async (req, res, next) => {
       break
     case -2:
       req.flash('noti', [
-        '承認依頼',
+        '支払依頼',
         `仕訳情報設定が完了できませんでした。<BR>※明細ID「${lineId}」の補助科目「${subAccountCode}」は未登録補助科目です。`,
         'SYSERR'
       ])
@@ -186,7 +186,7 @@ const cbPostApprove = async (req, res, next) => {
       break
     case -3:
       req.flash('noti', [
-        '承認依頼',
+        '支払依頼',
         `仕訳情報設定が完了できませんでした。<BR>※明細ID「${lineId}」の部門データ「${departmentCode}」は未登録部門データです。`,
         'SYSERR'
       ])
