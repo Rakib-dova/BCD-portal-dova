b454329570200938fcc2ce5ccdbf128bde0e1c4e
pro_hong <pro_hong@eclnttcom100009.onmicrosoft.com>
2022-03-11 19:12:39 +0900

    【PBI3586】【レビュー後修正】ST#4137

--- Application/DAO/ApprovalDAO.js
+++ Application/DAO/ApprovalDAO.js
@@ -11,6 +11,10 @@ class ApprovalDAO extends DAO {
     this.approveStatusDAO = require('./ApproveStatusDAO')
   }
 
+  /**
+   * 契約者の支払依頼の依頼承認を取得
+   * @returns promise<Model>
+   */
   async getApprovals() {
     const approvals = await this.DTO.findAll({
       order: [['approvedAt', 'DESC']]
@@ -18,6 +22,10 @@ class ApprovalDAO extends DAO {
     return approvals
   }
 
+  /**
+   * 承認待ちになっている依頼決済データを取得
+   * @returns promise<mode>
+   */
   async getWaitingApprovals() {
     const workflowDone = await this.approveStatusDAO.getStautsCode('最終承認済み')
     const rejectWorkflow = await this.approveStatusDAO.getStautsCode('差し戻し')
