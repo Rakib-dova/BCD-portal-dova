b1ac18463c030ff2715d67b2c91b03a28f8931fd
dongwoo kang <kang@eclnttcom000670.onmicrosoft.com>
2022-03-14 12:03:01 +0900

    【ST4138】【DBテーブル修正】FK解除_Approvalテーブルの解除漏れ対応

--- Application/migrations/20220311111758-remove_constraint_fk_approveRouteId.js
+++ Application/migrations/20220311111758-remove_constraint_fk_approveRouteId.js
@@ -1,8 +1,22 @@
 'use strict'
-const fkName = 'FK__RequestAp__appro__37FA4C37'
+/**
+ * fkNameForRequestApproval : RequestApprovalテーブルに設定されている外部キーのNameを指定（「FK__RequestAp__appro__」で始まるキー）
+ * fkNameForApproval : Approvalテーブルに設定されている外部キーのNameを指定（「FK__Approval__approv__」で始まるキー）
+ *
+ * ★★★★★★★★★★★★※「FK__Approval__approv__」で始まるキーが複数ある場合の指定方法
+ *                             １．SQLServerのApprovaテーブルのキーフォルダ確認
+ *                             ２．「FK__Approval__approv__」で始まるキーを右クリックし、「Script as Create」をクリック
+ *                             ３．下記のSQLが表示されるキーのNameを指定。
+ *                                   ALTER TABLE [dbo].[Approval]  WITH CHECK ADD FOREIGN KEY([approveRouteId]) REFERENCES [dbo].[ApproveRoute] ([approveRouteId])
+ */
+const fkNameForRequestApproval = 'FK__RequestAp__appro__37FA4C37'
+const fkNameForApproval = 'FK__Approval__approv__1E05700A'
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return [await queryInterface.removeConstraint('RequestApproval', fkName)]
+    return [
+      await queryInterface.removeConstraint('RequestApproval', fkNameForRequestApproval),
+      await queryInterface.removeConstraint('Approval', fkNameForApproval)
+    ]
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -10,7 +24,18 @@ module.exports = {
       await queryInterface.addConstraint('RequestApproval', {
         fields: ['approveRouteId'],
         type: 'foreign key',
-        name: fkName,
+        name: fkNameForRequestApproval,
+        references: {
+          table: 'ApproveRoute',
+          field: 'approveRouteId'
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      }),
+      await queryInterface.addConstraint('Approval', {
+        fields: ['approveRouteId'],
+        type: 'foreign key',
+        name: fkNameForApproval,
         references: {
           table: 'ApproveRoute',
           field: 'approveRouteId'
