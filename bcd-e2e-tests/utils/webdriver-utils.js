896011e56b0a6601f894d4ad87b6cf75924e569c
toshiyuki hikita <pro_hikita@eclnttcom100009.onmicrosoft.com>
2022-03-17 01:29:00 +0000

    Updated webdriver-utils.js

--- bcd-e2e-tests/utils/webdriver-utils.js
+++ bcd-e2e-tests/utils/webdriver-utils.js
@@ -40,6 +40,7 @@ exports.openFirefox = openFirefox;
 function setReporter() {
   // 初回実行時のみ生成
   if (!isReporterCreated) {
+    autotest_report_config.enableScreenshot = process.env.ENABLE_SCREEN_SHOT;
     const reporter = new wr_test_reporter_1.WrTestReporter(autotest_report_config);
     jasmine.getEnv().addReporter(new wr_reporter_jasmine_adaptor_1.WrReporterJasmineAdaptor(reporter));
     isReporterCreated = true;
