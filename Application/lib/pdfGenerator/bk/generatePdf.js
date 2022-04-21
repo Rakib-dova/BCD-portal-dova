bd5b757c37533194ecbc7704bf8c9b2b310ef0bb
tomoyuki kanno <prod_A101097@eclnttcom100009.onmicrosoft.com>
2022-04-21 19:49:45 +0900

    モジュール読み込み不具合を修正

--- Application/lib/pdfGenerator/bk/generatePdf.js
+++ Application/lib/pdfGenerator/bk/generatePdf.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer')
+// const puppeteer = require('puppeteer')
 
 const defaultOptions = {
   format: 'A4'
@@ -6,30 +6,30 @@ const defaultOptions = {
   // height: 1100
 }
 
-const generatePdf = async (html, options = defaultOptions) => {
-  const args = [
-    '--no-sandbox',
-    '--disable-setuid-sandbox'
-  ]
+// const generatePdf = async (html, options = defaultOptions) => {
+//   const args = [
+//     '--no-sandbox',
+//     '--disable-setuid-sandbox'
+//   ]
 
-  console.log('== ブラウザ起動 ==================')
-  const browser = await puppeteer.launch({
-    ignoreHTTPSErrors: true,
-    args: args
-  })
-  console.log('== ページ表示開始 ==================')
-  const page = await browser.newPage()
+//   console.log('== ブラウザ起動 ==================')
+//   const browser = await puppeteer.launch({
+//     ignoreHTTPSErrors: true,
+//     args: args
+//   })
+//   console.log('== ページ表示開始 ==================')
+//   const page = await browser.newPage()
 
-  await page.setContent(html, {
-    waitUntil: 'networkidle0'
-  })
+//   await page.setContent(html, {
+//     waitUntil: 'networkidle0'
+//   })
 
-  const data = await page.pdf(options).finally(async () => {
-    await browser.close()
-    console.log('== ブラウザ停止 ===================')
-  })
+//   const data = await page.pdf(options).finally(async () => {
+//     await browser.close()
+//     console.log('== ブラウザ停止 ===================')
+//   })
 
-  return Buffer.from(Object.values(data))
-}
+//   return Buffer.from(Object.values(data))
+// }
 
-module.exports = generatePdf
+// module.exports = generatePdf
--- Application/lib/pdfGenerator/generatePdf.js
+++ Application/lib/pdfGenerator/generatePdf.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer')
+// const puppeteer = require('puppeteer')
 const { chromium } = require('playwright');
 
 const defaultOptions = {
