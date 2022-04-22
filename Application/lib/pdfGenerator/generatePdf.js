e18650d2e1a518982574c80906d1403e18e874ec
tomoyuki kanno <prod_A101097@eclnttcom100009.onmicrosoft.com>
2022-04-22 11:39:10 +0900

    PDF出力をpuppeteer形式に戻す

--- Application/lib/pdfGenerator/generatePdf.js
+++ Application/lib/pdfGenerator/generatePdf.js
@@ -1,23 +1,10 @@
-// const puppeteer = require('puppeteer')
-// const { chromium } = require('playwright');
-const { chromium } = require("playwright-chromium")
+const puppeteer = require('puppeteer')
 
 const defaultOptions = {
   format: 'A4'
   // width: 1300,
   // height: 1100
 }
-
-
-
-
-// (async () => {
-//   const browser = await chromium.launch()
-//   const page = await browser.newPage()
-//   await page.goto('https://checklyhq.com/learn/headless')
-//   await page.pdf({ path: 'checkly.pdf' })
-//   await browser.close()
-// })()
 
 const generatePdf = async (html, options = defaultOptions) => {
   const args = [
@@ -26,12 +13,10 @@ const generatePdf = async (html, options = defaultOptions) => {
   ]
 
   console.log('== ブラウザ起動 ==================')
-  const browser = await chromium.launch()
-  // const browser = await chromium.launch({
-  //   ignoreHTTPSErrors: true,
-  //   args: args
-  // })
-
+  const browser = await puppeteer.launch({
+    ignoreHTTPSErrors: true,
+    args: args
+  })
   console.log('== ページ表示開始 ==================')
   const page = await browser.newPage()
 
@@ -46,32 +31,5 @@ const generatePdf = async (html, options = defaultOptions) => {
 
   return Buffer.from(Object.values(data))
 }
-
-
-// const generatePdf = async (html, options = defaultOptions) => {
-//   const args = [
-//     '--no-sandbox',
-//     '--disable-setuid-sandbox'
-//   ]
-
-//   console.log('== ブラウザ起動 ==================')
-//   const browser = await puppeteer.launch({
-//     ignoreHTTPSErrors: true,
-//     args: args
-//   })
-//   console.log('== ページ表示開始 ==================')
-//   const page = await browser.newPage()
-
-//   await page.setContent(html, {
-//     waitUntil: 'networkidle0'
-//   })
-
-//   const data = await page.pdf(options).finally(async () => {
-//     await browser.close()
-//     console.log('== ブラウザ停止 ===================')
-//   })
-
-//   return Buffer.from(Object.values(data))
-// }
 
 module.exports = generatePdf
