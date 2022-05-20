566822ff16644c50af719be6f7014e7800ba612b
root <root@gs042.cse_hn_domain.cseltd.co.jp>
2022-05-20 16:02:34 +0900

    [PB#4544] ST#4771【実装】ユーザー一括作成画面作成

--- Application/routes/uploadUsers.js
+++ Application/routes/uploadUsers.js
@@ -12,6 +12,72 @@ const constantsDefine = require('../constants')
 const upload = require('multer')({ dest: process.env.INVOICE_UPLOAD_PATH })
 const uploadUsersController = require('../controllers/uploadUsersController')
 
+const cbGetIndex = async (req, res, next) => {
+  logger.info(constantsDefine.logMessage.INF000 + 'cbGetIndex')
+  // 認証情報取得処理
+  if (!req.session || !req.user?.userId) {
+    return next(errorHelper.create(500))
+  }
+
+  // DBからuserデータ取得
+  const user = await userController.findOne(req.user.userId)
+  // データベースエラーは、エラーオブジェクトが返る
+  // user未登録の場合もエラーを上げる
+  if (user instanceof Error || user === null) return next(errorHelper.create(500))
+
+  // TX依頼後に改修、ユーザステイタスが0以外の場合、「404」エラーとする not 403
+  if (user.dataValues?.userStatus !== 0) return next(errorHelper.create(404))
+
+  // DBから契約情報取得
+  const contract = await contractController.findOne(req.user.tenantId)
+  // データベースエラーは、エラーオブジェクトが返る
+  // 契約情報未登録の場合もエラーを上げる
+  if (contract instanceof Error || contract === null) return next(errorHelper.create(500))
+
+  req.session.userContext = 'LoggedIn'
+
+  // ユーザ権限を取得
+  req.session.userRole = user.dataValues?.userRole
+  const deleteFlag = contract.dataValues.deleteFlag
+  const contractStatus = contract.dataValues.contractStatus
+  const checkContractStatus = await helper.checkContractStatus(req.user.tenantId)
+
+  if (checkContractStatus === null || checkContractStatus === 999) {
+    return next(errorHelper.create(500))
+  }
+
+  if (!validate.isStatusForCancel(contractStatus, deleteFlag)) {
+    return next(noticeHelper.create('cancelprocedure'))
+  }
+
+  const procedureContents = {
+    procedureTitle: '(手順)',
+    procedureComment1: '1. 下記リンクをクリックし、アップロード用のCSVファイルをダウンロード',
+    procedureComment2: '2. CSVファイルにユーザーデータを記入',
+    procedureComment2Children: [
+      'A列：メールアドレス',
+      'B列：ロール(数字)',
+      '※1ファイルで作成できるユーザーデータの数は００まで',
+      '※文字コードはUTF-8 BOM付で作成してください'
+    ],
+    procedureComment3: '3.「ファイル選択」ボタンをクリックし、記入したCSVファイルを選択',
+    procedureComment4: '4.「アップロード開始」ボタンをクリック'
+  }
+
+  // アップロードフォーマットデータを画面に渡す。
+  res.render('uploadUsers', {
+    uploadCommonLayoutTitle: 'ユーザー一括作成',
+    uploadCommonLayoutEngTitle: 'BULK UPLOAD USERS',
+    fileInputName: 'userNameFileUpload',
+    cautionForSelectedFile: 'ファイルを選択してください。',
+    accountCodeUpload: '/uploadUsers',
+    procedureContents: procedureContents,
+    formatFileLocation: '../html/ユーザー括作成フォーマット.csv',
+    formatFileLinkText: 'アップロード用CSVファイルダウンロード'
+  })
+  logger.info(constantsDefine.logMessage.INF001 + 'cbGetIndex')
+}
+
 const cbPostIndex = async (req, res, next) => {
   logger.info(constantsDefine.logMessage.INF000 + 'uploadUsers.cbPostIndex')
   // 認証情報取得処理
@@ -85,11 +151,11 @@ const cbPostIndex = async (req, res, next) => {
   res.redirect('/uploadUsers')
 }
 
-// router.get('/', helper.isAuthenticated, cbGetIndex)
+router.get('/', helper.isAuthenticated, cbGetIndex)
 router.post('/', helper.isAuthenticated, upload.single('userNameFileUpload'), cbPostIndex)
 
 module.exports = {
   router: router,
-  // cbGetIndex: cbGetIndex,
+  cbGetIndex: cbGetIndex,
   cbPostIndex: cbPostIndex
 }
