5060e706953eed3931574247197ff674a9ab87fc
kion mou <pro_mou@eclnttcom100009.onmicrosoft.com>
2022-06-16 07:54:38 +0000

    Revert "ライトプラン"

--- Application/public/js/cancelLightPlan-page.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('cancelltion-button').onclick = function () {
-  const modal = document.getElementById('cancellation-modal')
-  if (modal) modal.classList.toggle('is-active')
-}
