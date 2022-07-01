3fb6b257b5f736f8204060be2f06de1faa43369e
tommorisawa <tommorisawa@cseltd.co.jp>
2022-07-01 16:07:00 +0900

    【PBI4952】【実装】ST#5228 承認件数取得APIをHome画面に適用

--- Application/public/js/portal-page.js
+++ Application/public/js/portal-page.js
@@ -1,3 +1,4 @@
+import BconAxios from './lib/BconAxios.js'
 // UserAgentで判定し
 // IE以外は動的にスクリプトをロード
 const ua = window.navigator.userAgent
@@ -25,4 +26,66 @@ window.onload = () => {
       }
     })
   })
+}
+
+const config = {
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+const result = BconAxios.get('/api/noticeCount', config)
+result.then((res) => {
+  const errorStatus = res.status
+  switch (errorStatus) {
+    case 403:
+      document.querySelector('.approvalNotificationMessage').innerText = 'ログインユーザーではありません。'
+      return
+    case 500:
+      document.querySelector('.approvalNotificationMessage').innerText = 'システムエラーが発生しました。'
+      return
+  }
+
+  const data = res.data.split(',')
+  createNotification(data[0], data[1])
+})
+
+function createElement(cnt, request, link) {
+  const approvalNotification = document.createElement('div')
+  const approvalNotificationMessage = document.createElement('div')
+  const approvalNotificationCnt = document.createElement('div')
+  const approvalNotificationLink = document.createElement('a')
+
+  approvalNotificationCnt.innerText = `${cnt}`
+  approvalNotificationCnt.classList.add('approvalNotificationCnt')
+  approvalNotificationMessage.innerText = `${request}が`
+  approvalNotificationMessage.classList.add('approvalNotificationMessage')
+
+  approvalNotificationLink.setAttribute('href', link)
+  approvalNotificationLink.innerText = '確認'
+  approvalNotificationLink.classList.add('approvalNotificationLink')
+
+  approvalNotificationMessage.append(approvalNotificationCnt)
+  approvalNotificationMessage.append('件あります。')
+  approvalNotification.append(approvalNotificationMessage)
+  approvalNotification.append(approvalNotificationLink)
+  approvalNotification.classList.add('approvalNotification')
+  return approvalNotification
+}
+
+function createNotification(requestNoticeCnt, rejectedNoticeCnt) {
+  if (~~requestNoticeCnt <= 0 && ~~rejectedNoticeCnt <= 0) {
+    document.querySelector('.approvalNotificationMessage').innerText = '承認・差し戻し依頼はありません。'
+    return
+  }
+
+  document.querySelector('.approvalNotification').remove()
+
+  if (~~requestNoticeCnt > 0) {
+    const requestNotice = createElement(requestNoticeCnt, '支払依頼', '/inboxList/approvals')
+    document.querySelector('.column.is-12.menu').append(requestNotice)
+  }
+  if (~~rejectedNoticeCnt > 0) {
+    const rejectedNotice = createElement(rejectedNoticeCnt, '差し替え依頼', '/inboxList/1')
+    document.querySelector('.column.is-12.menu').append(rejectedNotice)
+  }
 }
