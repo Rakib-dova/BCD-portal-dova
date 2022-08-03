84b78fc90ea9ba638e2194157d46fd3e0dd583e2
erina-kon <kon@actec2.omicrosoft.com>
2022-08-03 15:03:41 +0900

    PBI#4941 印影登録処理の修正

--- Application/lib/storageCommon.js
+++ Application/lib/storageCommon.js
@@ -16,39 +16,42 @@ const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_C
 // コンテナ接続
 const containerClient = blobServiceClient.getContainerClient('imprint')
 
-const upload = (blobName) => {
-  logger.info(constantsDefine.logMessage.INF000 + 'storageCommon getClient')
+const upload = (tenantId, data) => {
+  logger.info(constantsDefine.logMessage.INF000 + 'storageCommon upload')
 
   // Get a block blob client
-  const blockBlobClient = containerClient.getBlockBlobClient(blobName)
+  const blockBlobClient = containerClient.getBlockBlobClient(tenantId)
 
-  console.log('\nUploading to Azure storage as blob:\n\t', blobName)
+  console.log('\nUploading to Azure storage as blob:\n\t', tenantId)
 
-  // Upload data to the blob
-  const data = 'Hello, World!'
   const result = blockBlobClient.upload(data, data.length)
 
-  logger.info(constantsDefine.logMessage.INF001 + 'storageCommon getClient')
+  logger.info(constantsDefine.logMessage.INF001 + 'storageCommon upload')
   return result
 }
 
 const getImprint = (tenantId) => {
-  logger.info(constantsDefine.logMessage.INF000 + 'storageCommon getClient')
+  logger.info(constantsDefine.logMessage.INF000 + 'storageCommon getImprint')
+  // Get a block blob client
+  const blockBlobClient = containerClient.getBlockBlobClient(tenantId)
   const downloadBlockBlobResponse = blockBlobClient.download(0)
+  const result = streamToText(downloadBlockBlobResponse.readableStreamBody)
+
   console.log('\nDownloaded blob content...')
   console.log('\t', streamToText(downloadBlockBlobResponse.readableStreamBody))
 
-  // Convert stream to text
-  async function streamToText(readable) {
-    readable.setEncoding('utf8')
-    let data = ''
-    for await (const chunk of readable) {
-      data += chunk
-    }
-    return data
-  }
   logger.info(constantsDefine.logMessage.INF001 + 'storageCommon getClient')
   return result
 }
+
+async function streamToText(readable) {
+  readable.setEncoding('utf8')
+  let data = ''
+  for await (const chunk of readable) {
+    data += chunk
+  }
+  return data
+}
+
 exports.upload = upload
 exports.getImprint = getImprint
