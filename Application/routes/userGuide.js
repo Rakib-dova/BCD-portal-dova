828331097c707b35880231e4c9938765e362c821
pro_rakib <pro_rakib@eclnttcom100009.onmicrosoft.com>
2022-09-08 11:42:13 +0900

    【PBI5664】【製造】PBI#5664_マニュアル導線改善作業

---
+++ Application/routes/userGuide.js
@@ -0,0 +1,65 @@
+'use strict'
+const express = require('express')
+const router = express.Router()
+const helper = require('./helpers/middleware')
+const validate = require('../lib/validate')
+const errorHelper = require('./helpers/error')
+const noticeHelper = require('./helpers/notice')
+const userController = require('../controllers/userController.js')
+const contractController = require('../controllers/contractController.js')
+const logger = require('../lib/logger')
+const constantsDefine = require('../constants')
+
+
+const cbGetIndex = async (req, res, next) => {
+  logger.info(constantsDefine.logMessage.INF000 + 'cbGetIndex')
+  // 認証情報取得処理
+  if (!req.session || !req.user?.userId) {
+    return next(errorHelper.create(500))
+  }
+
+  // DBからuserデータ取得
+  const user = await userController.findOne(req.user.userId)
+  // データベースエラーは、エラーオブジェクトが返る
+  // user未登録の場合もエラーを上げる
+  if (user instanceof Error || user === null) return next(errorHelper.create(500))
+
+  // TX依頼後に改修、ユーザステイタスが0以外の場合、「404」エラーとする not 403
+  if (user.dataValues?.userStatus !== 0) return next(errorHelper.create(404))
+
+  // DBから契約情報取得
+  const contract = await contractController.findOne(req.user.tenantId)
+  // データベースエラーは、エラーオブジェクトが返る
+  // 契約情報未登録の場合もエラーを上げる
+  if (contract instanceof Error || contract === null) return next(errorHelper.create(500))
+
+  req.session.userContext = 'LoggedIn'
+
+  // ユーザ権限を取得
+  req.session.userRole = user.dataValues?.userRole
+  const deleteFlag = contract.dataValues.deleteFlag
+  const contractStatus = contract.dataValues.contractStatus
+  const checkContractStatus = await helper.checkContractStatus(req.user.tenantId)
+
+  if (checkContractStatus === null || checkContractStatus === 999) {
+    return next(errorHelper.create(500))
+  }
+
+  if (!validate.isStatusForCancel(contractStatus, deleteFlag)) {
+    return next(noticeHelper.create('cancelprocedure'))
+  }
+
+  // ご契約内容を画面に渡す。
+  res.render('userGuide', {
+    title: 'ご利用ガイド',
+    engTitle: 'USER GUIDE',
+  })
+  logger.info(constantsDefine.logMessage.INF001 + 'cbGetIndex')
+}
+
+router.get('/', helper.isAuthenticated, cbGetIndex)
+
+module.exports = {
+  router: router,
+  cbGetIndex: cbGetIndex
+}
